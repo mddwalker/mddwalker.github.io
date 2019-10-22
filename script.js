a730673fa186d3c332c7f71d6ff86e79b4555249
mddwalker <mddwalker@gmail.com>
2019-10-21 22:46:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,5 @@ var animation = bodymovin.loadAnimation(
 		path: 'data.json'
 	})
 
-function mouseOver() {
- var animation = bodymovin.loadAnimation(
-	{
-		container: document.getElementById ('anim'),
-		renderer: 'svg',
-		loop: true,
-		path: 'data.json'
-	})
- }
 
-function mouseOut() {
-  document.getElementById('div').style.color = "black";
-
-}
 		
